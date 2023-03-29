fcb55f828191f76b7a48f2eaeb250f4ee6073be9
PaoAranda <125483278+PaoAranda@users.noreply.github.com>
2023-03-29 11:33:48 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+#MTA4MjI5NTcyNzYyOTI3NTIxNg.GuEksk.BBJg6S5D9orUAkaIFsqWkISyHB2O02lAKCCoIY
+import os
+import discord
+from discord.ext import commands
+import pytube
+import openai 
+TOKEN = 'MTA4MjI5NTcyNzYyOTI3NTIxNg.GuEksk.BBJg6S5D9orUAkaIFsqWkISyHB2O02lAKCCoIY'
+openai.api_key = "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"
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+  print('Bot conectado a discord')
+
+@client.event
+async def on_message(message):
+  if message.content.startswith("!hello"):
+    await message.channel.send("holi")
+  
+  if message.content.startswith("!sumar"):
+    numeros = message.content.split()[1:]
+    resultado= sum([float(num) for num in numeros])
+    await message.channel.send(f"El resultado de la suma es: {resultado}")
+
+  if message.content.startswith("!download"):
+    url = message.content.split()[1]
+    yt = pytube.YouTube(url)
+   
+    stream=yt.streams.get_highest_resolution()
+    filename = stream.download()
+    await message.channel.send(f'video descargando como:{filename}')
+    os.system(f"start {filename}")
+
+  if message.content.startswith("!AI"):
+    prompt = message.content 
+    response = openai.completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=2048)
+    answer=response.choices [0].text 
+    await message.channel.send(answer)
+    
+client.run(TOKEN)
